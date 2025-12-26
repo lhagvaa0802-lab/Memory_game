53828c7afe23c0d8bf9a3bdba321f93b8aa36904
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-12-26 11:34:40 +0800

    final

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
 const boxes = document.getElementsByClassName("box");
 const containerBox = document.getElementsByClassName("container_box");
 
-let compare = [];
-for (let i = 0; i < boxes.length; i++) {
-  const box = boxes[i];
+// let compare = [];
+// for (let i = 0; i < boxes.length; i++) {
+//   const box = boxes[i];
 
-  box.addEventListener("click", () => {
-    compare.push({
-      element: box,
-      value: box.textContent,
-    });
+//   box.addEventListener("click", () => {
+//     compare.push({
+//       element: box,
+//       value: box.textContent,
+//     });
 
-    if (compare.length === 2) {
-      checker();
-      compare = [];
-      console.log(compare);
-    }
-  });
-}
+//     if (compare.length === 2) {
+//       checker();
+//       compare = [];
+//       console.log(compare);
+//     }
+//   });
+// }
 
-const checker = () => {
-  if (compare[0].value === compare[1].value) {
-    console.log((compare[0].element.textContent = "0"));
-    console.log((compare[1].element.textContent = "0"));
-  }
-};
+// const checker = () => {
+//   if (compare[0].value === compare[1].value) {
+//     console.log((compare[0].element.textContent = "0"));
+//     console.log((compare[1].element.textContent = "0"));
+//   }
+// };
+
+boxes = [
+  {
+    element: box,
+    value: box.textContent,
+  },
+  {
+    element: box,
+    value: box.textContent,
+  },
+  {
+    element: box,
+    value: box.textContent,
+  },
+  {
+    element: box,
+    value: box.textContent,
+  },
+];
 
 const randomGenerotor = () => {
-  const randomIndex = Math.floor(Math.random * obj.length);
+  const box = boxes[Math.floor(Math.random() * items.length)];
 };
